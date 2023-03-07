a4fe6a115d3b90f96b9d6061688c7048512aa363
thanhdat4461 <125394392+thanhdat4461@users.noreply.github.com>
2023-03-07 21:23:59 +0700

    Update Main Slap Battles.lua

--- Main Slap Battles.lua
+++ Main Slap Battles.lua
@@ -84,6 +84,10 @@ Tab1Section:NewSlider("Fov", "20-120", 120, 20, function(v) -- 120 (MaxValue) | 
 
 end)
 
+Tab2Section:NewButton("Click TP Tools","FE Click TP",function()
+mouse = game.Players.LocalPlayer:GetMouse() tool = Instance.new("Tool") tool.RequiresHandle = false tool.Name = "Equip to Click TP" tool.Activated:connect(function() local pos = mouse.Hit+Vector3.new(0,2.5,0) pos = CFrame.new(pos.X,pos.Y,pos.Z) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos end) tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+		
 Tab2Section:NewButton("Infinite Yield","FE Infinite Yield",function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
