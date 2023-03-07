77b2183a61ad404dc8cb6d8a3070047ebd20688f
thanhdat4461 <125394392+thanhdat4461@users.noreply.github.com>
2023-03-07 23:01:26 +0700

    Update Main Slap Battles.lua

--- Main Slap Battles.lua
+++ Main Slap Battles.lua
@@ -60,6 +60,13 @@ local Tab6Section = Tab6:NewSection("LocalPlayer Made By BkuIl Hub")
 
 local Tab6Section = Tab6:NewSection("Slap Aura Made By Anakin")
 
+local Tab6Section = Tab6:NewSection("Auto Get Tycoon Made By Anakin")
+
+local Tab6Section = Tab6:NewSection("Auto Get Elude Made By BluB")
+
+local Tab6Section = Tab6:NewSection("Auto Get [REDACTED] Made By Anakin")
+
+
 ---Buttons
 
 ---Toggles
@@ -84,8 +91,84 @@ Tab1Section:NewSlider("Fov", "20-120", 120, 20, function(v) -- 120 (MaxValue) | 
 
 end)
 
-Tab2Section:NewButton("Click TP Tools","FE Click TP",function()
+Tab1Section:NewButton("Click TP Tools","FE Click TP",function()
 mouse = game.Players.LocalPlayer:GetMouse() tool = Instance.new("Tool") tool.RequiresHandle = false tool.Name = "Equip to Click TP" tool.Activated:connect(function() local pos = mouse.Hit+Vector3.new(0,2.5,0) pos = CFrame.new(pos.X,pos.Y,pos.Z) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos end) tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
+Tab2Section:NewButton("AntiLag","FE AntiLag",function()
+local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
+
+local g = game
+
+local w = g.Workspace
+
+local l = g.Lighting
+
+local t = w.Terrain
+
+t.WaterWaveSize = 0
+
+t.WaterWaveSpeed = 0
+
+t.WaterReflectance = 0
+
+t.WaterTransparency = 0
+
+l.GlobalShadows = false
+
+l.FogEnd = 9e9
+
+l.Brightness = 0
+
+settings().Rendering.QualityLevel = "Level01"
+
+for i, v in pairs(g:GetDescendants()) do
+
+    if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+
+        v.Material = "Plastic"
+
+        v.Reflectance = 0
+
+    elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+
+        v.Transparency = 1
+
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+
+        v.Lifetime = NumberRange.new(0)
+
+    elseif v:IsA("Explosion") then
+
+        v.BlastPressure = 1
+
+        v.BlastRadius = 1
+
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") then
+
+        v.Enabled = false
+
+    elseif v:IsA("MeshPart") then
+
+        v.Material = "Plastic"
+
+        v.Reflectance = 0
+
+        v.TextureID = 10385902758728957
+
+    end
+
+end
+
+for i, e in pairs(l:GetChildren()) do
+
+    if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+
+        e.Enabled = false
+
+    end
+
+end
 end)
 		
 Tab2Section:NewButton("Infinite Yield","FE Infinite Yield",function()
@@ -461,7 +544,7 @@ game:GetService("TeleportService"):Teleport(11828384869)
 
 end)
 
-Tab4Section:NewButton("Auto Get Redacted","Free Redacted",function()
+Tab4Section:NewButton("Auto Get [REDACTED]","Free Redacted",function()
 
     local Door = 1
 
