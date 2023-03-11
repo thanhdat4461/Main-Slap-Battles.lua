5bb6a6a863b4df808278963ca72f0c77c4507491
thanhdat4461 <125394392+thanhdat4461@users.noreply.github.com>
2023-03-11 15:52:32 +0700

    Update Main Slap Battles.lua

--- Main Slap Battles.lua
+++ Main Slap Battles.lua
@@ -91,8 +91,6 @@ local Tab5Section = Tab5:NewSection("Teleport To Lobby By ThAGqoBSGE#2962")
 
 local Tab5Section = Tab5:NewSection("Teleport To Tournament By ThAGqoBSGE#2962")
 
-_G.AntiTimestop = false;
-
 ---Buttons
 
 ---Toggles
@@ -1275,9 +1273,55 @@ Tab2Section:NewToggle("Anti Stun","No Longer Getting Stun",function(bool)
     end)
 
 Tab2Section:NewToggle("Anti TimeStop", "No Longer Dying Form GodHand User", function(bool)
-    _G.AntiTimestop = bool;
+ 
+	AntiTimes = bool
 
-end)
+
+
+        if bool == true then
+
+
+
+           while antiTime do
+
+				
+				
+           task.wait() do
+					
+					
+
+            if LocalPlayer.Character ~= nil and LocalPlayer.Character:FindFirstChildOfClass("Humanoid") ~= nil and LocalPlayer.Character:WaitForChild("Humanoid").Health > 0 and LocalPlayer.Character:WaitForChild("Torso").Transparency == 0 then
+
+                if game:GetService("Workspace"):FindFirstChild("universaltimestop") ~= nil then
+
+                    LocalPlayer.Character:FindFirstChildOfClass("Humanoid").PlatformStand = false;
+
+                    LocalPlayer.Character:FindFirstChild("TSVulnerability").Value = false;
+
+                    for _,v in pairs(LocalPlayer.Character:GetChildren()) do
+
+                        if v:IsA("MeshPart") or v:IsA("Part") then
+
+                            v.Anchored = false;
+
+                        end
+
+                    end
+
+                elseif not game:GetService("Workspace"):FindFirstChild("universaltimestop") then
+
+                    LocalPlayer.Character:FindFirstChildOfClass("Humanoid").PlatformStand = false;
+
+                    LocalPlayer.Character:FindFirstChild("TSVulnerability").Value = true;
+
+                end
+
+            end
+
+        end
+
+    end)
+
 
 Tab2Section:NewToggle("Anti Booster","No Longer Getting Banned Form Booster Bug",function(bool)
 
