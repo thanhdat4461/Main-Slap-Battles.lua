cebe025e22b873b301e52b0ebd00c1299d4e104a
thanhdat4461 <125394392+thanhdat4461@users.noreply.github.com>
2023-03-14 13:55:48 +0700

    Update Main Slap Battles.lua

--- Main Slap Battles.lua
+++ Main Slap Battles.lua
@@ -27,6 +27,28 @@
 
 
 
+local Namecall
+
+Namecall = hookmetamethod(game, '__namecall', function(self, ...)
+
+   if getnamecallmethod() == 'FireServer' and tostring(self) == 'Ban' then
+
+       return
+
+   elseif getnamecallmethod() == 'FireServer' and tostring(self) == 'WalkSpeedChanged' then
+
+       return
+
+   elseif getnamecallmethod() == 'FireServer' and tostring(self) == 'AdminGUI' then
+
+       return
+
+   end
+
+   return Namecall(self, ...)
+
+end)
+
 local kavoUi = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 
 local window = kavoUi.CreateLib("Main Slap Battles","Ocean")
